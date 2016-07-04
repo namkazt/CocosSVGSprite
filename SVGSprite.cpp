b18fe5f695c0a73f32cdd98c8623b809a2ac4e6e
Nguyen Dac Nam <nam.kazt.91@gmail.com>
2016-07-04 10:19:47 +0700

    Add Texture created to cache and load back
    Store texture in cached with key is path and params like scale, unit, dpi.
    when load another SVGSprite with same info. it will get from cache first

--- SVGSprite.cpp
+++ SVGSprite.cpp
@@ -19,42 +19,50 @@ SVGSprite* SVGSprite::create(std::string path, float scale, std::string unit, fl
 
 bool SVGSprite::init(std::string path, float scale, std::string unit, float dpi)
 {
-	NSVGimage *image = NULL;
-	NSVGrasterizer *rast = NULL;
-	unsigned char* img = NULL;
-	int w, h;
+	std::ostringstream os;
+	os << path << "_" << scale << "_" << unit << "_" << dpi;
+	Texture2D* texture = TextureCache::getInstance()->getTextureForKey(os.str());
+	if(texture != nullptr) return Sprite::initWithTexture(texture);
+	else {
+		NSVGimage *image = NULL;
+		NSVGrasterizer *rast = NULL;
+		unsigned char* img = NULL;
+		int w, h;
+		//--- parse svg from file
+		image = nsvgParseFromFile(path.c_str(), unit.c_str(), dpi);
+		if (image == NULL) {
+			log("Could not open SVG image.\n");
+			nsvgDeleteRasterizer(rast);
+			nsvgDelete(image);
+			return false;
+		}
+		//--- init rasterizer
+		w = (int)image->width * scale;
+		h = (int)image->height * scale;
+		rast = nsvgCreateRasterizer();
+		if (rast == NULL) {
+			log("Could not init Rasterizer.\n");
+			nsvgDeleteRasterizer(rast);
+			nsvgDelete(image);
+			return false;
+		}
+		//--- create image buffer
+		img = (unsigned char*)malloc(w*h * 4);
+		if (img == NULL) {
+			log("Could not alloc image buffer.\n");
+			nsvgDeleteRasterizer(rast);
+			nsvgDelete(image);
+			return false;
+		}
 
-	image = nsvgParseFromFile(path.c_str(), unit.c_str(), dpi);
-	if (image == NULL) {
-		log("Could not open SVG image.\n");
-		nsvgDeleteRasterizer(rast);
-		nsvgDelete(image);
-		return false;
+		log("rasterizing image %d x %d\n", w, h);
+		nsvgRasterize(rast, image, 0, 0, scale, img, w, h, w * 4);
+
+		Image* imageA = new Image();
+		imageA->initWithRawData(img, w*h * 4, w, h, 4);
+
+		texture = TextureCache::getInstance()->addUIImage(imageA, os.str());
+
+		return Sprite::initWithTexture(texture);
 	}
-
-	w = (int)image->width * scale;
-	h = (int)image->height * scale;
-	rast = nsvgCreateRasterizer();
-	if (rast == NULL) {
-		log("Could not init Rasterizer.\n");
-		nsvgDeleteRasterizer(rast);
-		nsvgDelete(image);
-		return false;
-	}
-
-	img = (unsigned char*)malloc(w*h * 4);
-	if (img == NULL) {
-		log("Could not alloc image buffer.\n");
-		nsvgDeleteRasterizer(rast);
-		nsvgDelete(image);
-		return false;
-	}
-
-	log("rasterizing image %d x %d\n", w, h);
-	nsvgRasterize(rast, image, 0, 0, scale, img, w, h, w * 4);
-
-	Texture2D* texture = new Texture2D();
-	texture->initWithData(img, w*h * 4, Texture2D::PixelFormat::RGBA8888, w, h, Size(w, h));
-
-	return Sprite::initWithTexture(texture);
 }
